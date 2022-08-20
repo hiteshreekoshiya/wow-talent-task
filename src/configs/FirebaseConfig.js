ccdf863fba4b1a9067ca8f2a650e0773bbb7d091
Hiteshree Koshiya <hiteshreekoshiya123@gmail.com>
2022-08-20 17:01:00 +0530

    initial commit

---
+++ src/configs/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const FirebaseConfig = {
+  apiKey: "AIzaSyD9skB6yZsWicI6cpjQKSZK83VAxZmkBKk",
+  authDomain: "wow-live-365f9.firebaseapp.com",
+  projectId: "wow-live-365f9",
+  storageBucket: "wow-live-365f9.appspot.com",
+  messagingSenderId: "157641636990",
+  appId: "1:157641636990:web:50c9167c51dfad8dd2ac2f",
+  measurementId: "G-1XYBWNPP1L"
+};
+
+export default FirebaseConfig
